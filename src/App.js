3257e3b42454e6a23a7a16591324b5454159395c
Vipin Negi <vipin@mindrops.com>
2022-12-03 10:47:32 +0530

    Onwards To A More Realistic Example

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://custom-hooks-17bc6-default-rtdb.firebaseio.com/tasks.json'
       );
 
-      if (!response.ok) {
+      if (!response.ok) { 
         throw new Error('Request failed!');
       }
 
